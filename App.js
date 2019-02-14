4c106a24eacad737b5e8f8f89820be85deedbf7e
Sotatek-ManhPham <manh.pham@sotatek.com>
2019-02-14 11:39:55 +0700

    Fix tan navigation

--- App.js
+++ App.js
@@ -1,53 +1,14 @@
 import React, { Component } from 'react';
 import {
-  View, Animated, Easing,
+  View,
 } from 'react-native';
-import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
-import Screens from './app/screens/Screens';
-import MainTabbar from './app/screens/MainTabbar';
+import MainScreen from './app/screens/MainScreen';
 
-const App = createStackNavigator(
-  Screens,
-  {
-    headerMode: 'screen',
-    cardStyle: { opacity: 1 },
-    transitionConfig: () => ({
-      transitionSpec: {
-        duration: 300,
-        easing: Easing.out(Easing.poly(4)),
-        timing: Animated.timing,
-      },
-      screenInterpolator: (sceneProps) => {
-        const { layout, position, scene } = sceneProps;
-        const { index } = scene;
-        const height = layout.initHeight;
-        const translateY = position.interpolate({
-          inputRange: [index - 1, index, index + 1],
-          outputRange: [height, 0, 0],
-        });
-        const opacity = position.interpolate({
-          inputRange: [index - 1, index - 0.99, index],
-          outputRange: [0, 1, 1],
-        });
-        return { opacity, transform: [{ translateY }] };
-      },
-    }),
-  },
-);
-
-const AppWithTabbar = createBottomTabNavigator(
-  { App }, {
-    tabBarPosition: 'bottom',
-    tabBarComponent: MainTabbar,
-  },
-);
-
-const Tabbar = createAppContainer(AppWithTabbar);
-export default class Root extends Component {
+export default class App extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <Tabbar />
+        <MainScreen />
       </View>
     );
   }
--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,75 +1,15 @@
 import React from 'react';
-import { createBottomTabNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import { Image } from 'react-native';
 import HomeScreen from './HomeScreen/HomeScreen';
 import CategoryScreen from './Category/CategoryScreen';
 
-const MainScreen = createBottomTabNavigator({
-  HomeScreen: {
-    screen: HomeScreen,
-    navigationOptions: () => ({
-      tabBarLabel: 'Home',
-    }),
-  },
-  CategorScreen: {
-    screen: CategoryScreen,
-    navigationOptions: () => ({
-      tabBarLabel: 'Categor',
-    }),
-  },
-  CarScreen: {
-    screen: HomeScreen,
-    navigationOptions: () => ({
-      tabBarLabel: 'Car',
-    }),
-  },
-  RequeScreen: {
-    screen: HomeScreen,
-    navigationOptions: () => ({
-      tabBarLabel: 'Reque',
-    }),
-  },
-  MyScreen: {
-    screen: HomeScreen,
-    navigationOptions: () => ({
-      tabBarLabel: 'My',
-    }),
-  },
-}, {
-  navigationOptions: ({ navigation }) => ({
-    tabBarIcon: ({ focused, tintColor }) => {
-      const { routeName } = navigation.state;
-      let iconName;
-      if (routeName === 'HomeScreen') {
-        focused ? (iconName = require('../assets/images/ic_home.png')) : (iconName = require('../assets/images/ic_home_not_active.png'));
-      } else if (routeName === 'CategorScreen') {
-        focused ? (iconName = require('../assets/images/ic_home.png')) : (iconName = require('../assets/images/ic_home_not_active.png'));
-      } else if (routeName === 'CarScreen') {
-        focused ? (iconName = require('../assets/images/ic_home.png')) : (iconName = require('../assets/images/ic_home_not_active.png'));
-      } else if (routeName === 'RequeScreen') {
-        focused ? (iconName = require('../assets/images/ic_home.png')) : (iconName = require('../assets/images/ic_home_not_active.png'));
-      } else if (routeName === 'MyScreen') {
-        focused ? (iconName = require('../assets/images/ic_home.png')) : (iconName = require('../assets/images/ic_home_not_active.png'));
-      }
-
-      return <Image resizeMode="contain" style={{ width: 15, height: 15 }} source={iconName} />;
-    },
-    gesturesEnabled: false,
-  }),
-  lazy: true,
-  tabBarComponent: () => null,
-  tabBarPosition: 'bottom',
-  tabBarOptions: {
-    activeTintColor: '#ffffff',
-    inactiveTintColor: '#aa6a01',
-    style: {
-      height: 50,
-    },
-    labelStyle: {
-      fontSize: 13,
-    },
-  },
-  animationEnabled: true,
-  swipeEnabled: false,
-  initialRouteName: 'HomeScreen',
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Category: CategoryScreen,
+  Cart: CategoryScreen,
+  Request: CategoryScreen,
+  Account: CategoryScreen,
 });
+
+export default createAppContainer(TabNavigator);
